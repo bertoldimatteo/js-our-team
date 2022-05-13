dd88511c573224100c735e9cbc0d4317750876a0
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-13 15:41:35 +0200

    aggiunto file js + istruzioni

---
+++ main.js
@@ -0,0 +1,9 @@
+// Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team. 
+// Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, 
+// stampare dinamicamente una card per ogni membro del team.
+// BONUS:
+// Utilizzare gli input presenti nella pagina per permettere all'utente 
+// di aggiungere nuovi membri del team: cliccando sul pulsante "add" viene creato un nuovo oggetto, 
+// il quale viene inserito nell'array iniziale e viene stampata una nuova card 
+// con tutte le informazioni inserite dall'utente.
